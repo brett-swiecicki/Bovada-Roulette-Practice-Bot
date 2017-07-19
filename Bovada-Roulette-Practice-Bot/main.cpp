e8587dd67f3bf019095f4e925babaafcf8210d02
Brett Swiecicki <brettcs@umich.edu>
2017-07-19 00:22:27 -0400

    Created project files.

---
+++ Bovada-Roulette-Practice-Bot/main.cpp
@@ -0,0 +1,17 @@
+// Brett Swiecicki
+// Project Started: 7/19/2017
+/*
+Description:
+Program automatically runs a user defined roulette strategy on Bovada in practice mode
+to confirm effectiveness (assuming practice mode runs identical to the real money mode).
+Effectiveness is determined by counting how many times the minimum required bankroll for
+the strategy doubles up or busts.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+}
