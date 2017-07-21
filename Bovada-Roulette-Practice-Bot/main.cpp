cc18e7683c0c3ec21234b9d9b8741c04b7a5b23d
Brett Swiecicki <brettcs@umich.edu>
2017-07-20 23:34:13 -0400

    Bot successfully opens practice mode and brings up the amounts desired in chrome developer tools.

--- Bovada-Roulette-Practice-Bot/main.cpp
+++ Bovada-Roulette-Practice-Bot/main.cpp
@@ -20,6 +20,10 @@ void printHeader();
 void getStratFromUser(vector<double>& strat_stakes);
 void closeBovada();
 void openDeveloperTools();
+void findHTMLvalues();
+void primeChromeForObjectSelction();
+void mouseMove(int x, int y);
+void leftClick();
 
 int main() {
 	vector<double> strat_stakes;
@@ -29,9 +33,11 @@ int main() {
 
 	//Open Bovada
 	ShellExecute(NULL, "open", "https://casino.bovada.lv/table-games/european-roulette?mode=practice", NULL, NULL, SW_SHOWMAXIMIZED);
-	Sleep(1000);
+	Sleep(7000); // Sleep to let page load. Hopefully there is a better way to do this!
 	openDeveloperTools();
-	Sleep(10000); //Sleep for 10 seconds to let page load. Hopefully there is a better way to do this!
+	Sleep(3000); // Sleep to let developer tools open
+	findHTMLvalues();
+	Sleep(10000);
 	
 	closeBovada();
 	cout << "Press ENTER to terminate bot." << endl;
@@ -129,4 +135,81 @@ void openDeveloperTools() { // CTRL + Shift + I
 	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
 	SendInput(1, &ip, sizeof(INPUT));
 
+}
+
+void findHTMLvalues() {
+	primeChromeForObjectSelction();
+	Sleep(2000);
+	mouseMove(80, 374);
+	leftClick();
+	Sleep(2000);
+	primeChromeForObjectSelction();
+	Sleep(2000);
+	mouseMove(230,374);
+	leftClick();
+}
+
+void primeChromeForObjectSelction() { // CTRL + Shift + C (prime Chrome to select HTML objects)
+	INPUT ip;
+	ip.type = INPUT_KEYBOARD;
+	ip.ki.wScan = 0; // hardware scan code for key
+	ip.ki.time = 0;
+	ip.ki.dwExtraInfo = 0;
+
+	// Press the "CTRL" key
+	ip.ki.wVk = VK_CONTROL; // virtual-key code for the "CTRL" key
+	ip.ki.dwFlags = 0; // 0 for key press
+	SendInput(1, &ip, sizeof(INPUT));
+
+	//Press the "Shift" key
+	ip.ki.wVk = VK_SHIFT;
+	ip.ki.dwFlags = 0; // 0 for key press
+	SendInput(1, &ip, sizeof(INPUT));
+
+	// Press the "C" key
+	ip.ki.wVk = 'C';
+	ip.ki.dwFlags = 0; // 0 for key press
+	SendInput(1, &ip, sizeof(INPUT));
+
+	// Release the "C" key
+	ip.ki.wVk = 'C';
+	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+	SendInput(1, &ip, sizeof(INPUT));
+
+	// Release the "Shift" key
+	ip.ki.wVk = VK_SHIFT;
+	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+	SendInput(1, &ip, sizeof(INPUT));
+
+	// Release the "CTRL" key
+	ip.ki.wVk = VK_CONTROL;
+	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+	SendInput(1, &ip, sizeof(INPUT));
+}
+
+void mouseMove(int x, int y) {
+	double fScreenWidth = ::GetSystemMetrics(SM_CXSCREEN) - 1;
+	double fScreenHeight = ::GetSystemMetrics(SM_CYSCREEN) - 1;
+	double fx = x*(65535.0f / fScreenWidth);
+	double fy = y*(65535.0f / fScreenHeight);
+	INPUT  Input = { 0 };
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+	Input.mi.dx = (long)fx;
+	Input.mi.dy = (long)fy;
+	::SendInput(1, &Input, sizeof(INPUT));
+}
+
+void leftClick() {
+	INPUT    Input = { 0 };
+	// left down 
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	::SendInput(1, &Input, sizeof(INPUT));
+
+	// left up
+	::ZeroMemory(&Input, sizeof(INPUT));
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+	::SendInput(1, &Input, sizeof(INPUT));
 }
